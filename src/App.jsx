2258eac8da72c0231ff98237a545303046699a30
Iryna <78929283+IrynaYrrr@users.noreply.github.com>
2023-11-01 13:15:09 +0100

    refactor Suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Outlet, Route, Routes } from 'react-router-dom'
 import Layout from 'Layout/Layout'
-import Home from 'pages/Home/Home'
 import NotFound from 'pages/NotFound/NotFound'
-import { Suspense, lazy } from 'react'
+import { lazy } from 'react'
 
 const MovieDetails = lazy(() => import('./pages/MovieDetails/MovieDetails'))
+
+const Home = lazy(() => import('pages/Home/Home'))
+
 const Cast = lazy(() => import('./components/Cast/Cast'))
 const Movies = lazy(() => import('./pages/Movies/Movies'))
 const Reviews = lazy(() => import('./components/Reviews/Reviews'))
@@ -14,21 +16,15 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Layout />}>
         <Route path='' element={<Home />} />
-        <Route path='movies' element={
-          <Suspense fallback={'Loading...'}>
-            <Movies />
-          </Suspense>}
-        />
+        <Route path='movies' element={<Movies />} />
         <Route path='movies/:movieId' element={
-          <Suspense fallback={'Loading...'}>
-            <>
-              <MovieDetails />
-              <Outlet />
-            </>
-          </Suspense>
+          <>
+            <MovieDetails />
+            <Outlet />
+          </>
         }>
-          <Route path='cast' element={<Suspense fallback={'Loading...'}><Cast /></Suspense>} />
-          <Route path='reviews' element={<Suspense fallback={'Loading...'}><Reviews /></Suspense>} />
+          <Route path='cast' element={<Cast />} />
+          <Route path='reviews' element={<Reviews />} />
         </Route>
         <Route path='*' element={<NotFound />} />
       </Route>
--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,13 +1,17 @@
-import { Outlet } from 'react-router-dom'
 import Header from 'components/Header/Header'
+import { Outlet } from 'react-router-dom'
+import { Suspense } from 'react'
 
 const Layout = () => {
-	return (
-		<>
-			<Header />
-			<Outlet />
-		</>
-	)
+  return (
+    <>
+      <Header />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Outlet />
+      </Suspense>
+
+    </>
+  )
 }
 
 export default Layout
