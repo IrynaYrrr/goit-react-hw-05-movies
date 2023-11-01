4e539c1da700081539be7dc60ae475ff60104952
Iryna <78929283+IrynaYrrr@users.noreply.github.com>
2023-11-01 20:06:29 +0100

    upd

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,6 +1,6 @@
 import { getFilmsBySearch } from 'api/getFilms'
 import MoviesList from 'components/MoviesList/MoviesList'
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSearchParams } from 'react-router-dom'
 
 const Movies = () => {
@@ -10,35 +10,33 @@ const Movies = () => {
   const [isLoading, setIsLoading] = useState(false)
   const [films, setFilms] = useState(null)
 
-  const fetchFilms = useCallback(async (query) => {
-    try {
-      setIsLoading(true)
-      setFilms(null)
-      const data = await getFilmsBySearch(query)
-      setFilms(data.results)
-    } catch (error) {
-      setError(error.response.data)
-    } finally {
-      setIsLoading(false)
-    }
-  }, [])
 
   const searchQuery = searchParams.get('search') ?? ''
 
-  const handleChange = ({ target: { value } }) => {
-    value ? setSearchParams({ search: value }) : setSearchParams({})
-  }
-
   const handleSubmit = (e) => {
     e.preventDefault()
-    setQuery(searchQuery)
-    fetchFilms(searchQuery)
+    setSearchParams({ search: query })
+  }
+
+  const handleChange = ({ target: { value } }) => {
+    setQuery(value)
   }
 
   useEffect(() => {
-    searchQuery && setQuery(searchQuery)
+    const fetchFilms = (async (query) => {
+      try {
+        setIsLoading(true)
+        setFilms(null)
+        const data = await getFilmsBySearch(query)
+        setFilms(data.results)
+      } catch (error) {
+        setError(error.response.data)
+      } finally {
+        setIsLoading(false)
+      }
+    })
     fetchFilms(searchQuery)
-  }, [])
+  }, [searchQuery])
 
   return (
     <>
@@ -50,10 +48,10 @@ const Movies = () => {
             <input
               name='title'
               type='text'
-              onChange={handleChange}
               className='form-control'
               id='exampleInputTodo'
-              value={searchQuery}
+              value={query}
+              onChange={handleChange}
             />
             <button>Search</button>
             {query && <MoviesList films={films} />}
